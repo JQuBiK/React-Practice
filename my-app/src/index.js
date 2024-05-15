f50cf8ab525d4dfafb4581513b6509806c354f79
Igor Rozhkov <freezof1x_1103@mai.ru>
2024-05-15 17:54:33 +0300

    Added files and folders for create new project

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,13 +1,13 @@
 import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App.js';
+import App from './components/app/app';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<StrictMode>
-		<App/>,
+		<App/>
 	</StrictMode>
 );
